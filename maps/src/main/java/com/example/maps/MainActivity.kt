5b4368a05f2824ec180613bb7076805d2298ab87
Ayushisingheka <ayushi.singh@ekasupport.com>
2024-09-29 00:49:48 +0530

    integrated maps

--- maps/src/main/java/com/example/maps/MainActivity.kt
+++ maps/src/main/java/com/example/maps/MainActivity.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.maps.databinding.ActivityMainBinding
+import com.ola.mapsdk.interfaces.OlaMapCallback
+import com.ola.mapsdk.view.OlaMap
+import com.ola.mapsdk.view.OlaMapView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mapView.getMap(apiKey = "QHaaGYVX6KfrAeakuS0N4nxvPuan5JEcf5X34TPE",
+            olaMapCallback = object : OlaMapCallback {
+                override fun onMapReady(olaMap: OlaMap) {
+                    // Map is ready to use
+                }
+
+                override fun onMapError(error: String) {
+                    // Handle map error
+                }
+            }
+        )
     }
 }
